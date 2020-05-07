4087ca27f8ac5123d4d2814414a6a2a1cde2885a
Akeel <akeelm_uk@hotmail.com>
2020-05-07 22:42:31 +0100

    not required

--- app.js
+++ app.js
@@ -19,9 +19,6 @@ mongoose.connect(
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  () => console.log("connected to db")
-});
 
 // ROUTES
 app.use('/api/jobs/', jobsRouter);
