187c91b032e1bbe24415673ddfa6c90209f504e5
Akeel <akeelm_uk@hotmail.com>
2020-05-10 12:41:11 +0100

    build stuff

--- server/server.js
+++ server/server.js
@@ -1,6 +1,18 @@
 const app = require('./app');
+const express = require('express');
+const path = require('path');
+
 require('dotenv/config');
 
 const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, './../client/build')));
+
+  app.get('*', function(req, res) {
+    console.log(path.join(__dirname, './../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, './../client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
